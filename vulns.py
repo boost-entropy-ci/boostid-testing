db6613e206a55a70d56ebf6e02229bb20c5c703e
François Proulx <francois@boostsecurity.io>
2022-01-28 00:06:40 -0500

    Adding some more findings

--- vulns.py
+++ vulns.py
@@ -2,9 +2,8 @@ import flask
 
 app = flask.Flask(__name__)
 
-@app.route("/error4")
-def error4(e):
-    # ruleid: python.flask.security.dangerous-template-string.dangerous-template-string
+@app.route("/route_a")
+def route_a():
     template = """
 {  extends "layout.html"  }
 {  block body  }
@@ -19,4 +18,38 @@ def error4(e):
 {  endblock  }
 """
     rendered = flask.render_template_string(template)
+
+@app.route("/route_a")
+def route_b():
+    template = """
+{  extends "DO_YOU_THINK_THIS_IS_DIFFERENT.html"  } 
+{  block body  }
+    <div class="center-content error">
+        <h1>Oops! That page doesn't exist.</h1>
+        <h3>
+"""
+    template += request.url
+    template += """
+</h3>
+</div>
+{  endblock  }
+"""
+    rendered = flask.render_template_string(template)
+
+@app.route("/route_a")
+def route_b():
+    ok_lets_make_it_different_template = """
+{  extends "layout.html"  }
+{  block body  }
+    <div class="center-content error">
+        <h1>Oops! That page doesn't exist.</h1>
+        <h3>
+"""
+    ok_lets_make_it_different_template += request.url
+    ok_lets_make_it_different_template += """
+</h3>
+</div>
+{  endblock  }
+"""
+    rendered = flask.render_template_string(ok_lets_make_it_different_template)
 
