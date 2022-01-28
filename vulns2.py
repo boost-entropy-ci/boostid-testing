f03f610602ccf446b9b50fb4d3d58ac7c9112f61
François Proulx <francois@boostsecurity.io>
2022-01-28 00:22:35 -0500

    Copy paste last vuln

--- vulns2.py
+++ vulns2.py
@@ -19,7 +19,7 @@ def route_a():
 """
     rendered = flask.render_template_string(template)
 
-@app.route("/route_a")
+@app.route("/route_b")
 def route_b():
     template = """
 {  extends "DO_YOU_THINK_THIS_IS_DIFFERENT.html"  } 
@@ -36,8 +36,8 @@ def route_b():
 """
     rendered = flask.render_template_string(template)
 
-@app.route("/route_a")
-def route_b():
+@app.route("/route_c")
+def route_c():
     ok_lets_make_it_different_template = """
 {  extends "layout.html"  }
 {  block body  }
@@ -53,3 +53,19 @@ def route_b():
 """
     rendered = flask.render_template_string(ok_lets_make_it_different_template)
 
+@app.route("/route_c_verbatim")
+def route_c_verbatim():
+    ok_lets_make_it_different_template = """
+{  extends "layout.html"  }
+{  block body  }
+    <div class="center-content error">
+        <h1>Oops! That page doesn't exist.</h1>
+        <h3>
+"""
+    ok_lets_make_it_different_template += request.url
+    ok_lets_make_it_different_template += """
+</h3>
+</div>
+{  endblock  }
+"""
+    rendered = flask.render_template_string(ok_lets_make_it_different_template)
