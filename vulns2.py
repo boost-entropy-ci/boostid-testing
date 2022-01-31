9f37841dfdbc5b026e7181296ae2ffe921d4608e
François Proulx <76956526+fproulx-boostsecurity@users.noreply.github.com>
2022-01-31 14:05:35 -0500

    Update vulns2.py

--- vulns2.py
+++ vulns2.py
@@ -52,3 +52,20 @@ def route_b():
 {  endblock  }
 """
     rendered = flask.render_template_string(ok_lets_make_it_different_template)
+    
+@app.route("/route_c_verbatim")
+def route_c_verbatim():
+    ok_lets_make_it_different_template = """
+{  extends "layout.html"  }
+{  block body  }
+    <div class="center-content error">
+        <h1>Oops! That page doesn't exist.</h1>
+        <h3>
+"""
+    ok_lets_make_it_different_template += request.url
+    ok_lets_make_it_different_template += """
+</h3>
+</div>
+{  endblock  }
+"""
+    rendered = flask.render_template_string(ok_lets_make_it_different_template)
